ab962690ddf4f212e171afea0f249642ec6f044b
Naveen Kumar Reddy Kota <naveenkumar.kota@rampgroup.com>
2022-06-13 07:21:42 -0700

    added facorial

---
+++ Main_Project/script.js
@@ -0,0 +1,17 @@
+function fun1() {
+    a = document.getElementById('num1').value;
+    fact = 1;
+    if(a=='')
+    {
+    document.getElementById('mess').innerHTML="please enter the number"
+    return false;
+    }
+    else{
+    for ( var i=1; i<=a; i++)
+    {
+    fact = fact * i;
+    }
+    document.getElementById('mess').innerHTML="the factorial is :" + fact;
+    return false;
+    }
+    }
