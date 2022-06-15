b8f45c4c9fdb453e0821ded14ef40a86b4cabfb8
Naveen Kumar Reddy Kota <naveenkumar.kota@rampgroup.com>
2022-06-15 00:18:46 -0700

    added css to js

--- Chart/script.js
+++ Chart/script.js
@@ -1,11 +1,25 @@
 const ctx = document.getElementById('myChart').getContext('2d');
+
+// global options
+
+Chart.defaults.global.defaultFontFamily ='Lato';
+Chart.defaults.global.defaultFontSize =18;
+Chart.defaults.global.defaultFontColor ='#777'
+
 const myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'bar',//bar,horizontal,radar,pie
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        labels: ['Boster', 'Worcester', 'Springfield', 'Lowell', 'Cambridge', 'New Bedford'],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'Population',
+            data: [
+                689,326,
+                185,186,
+                153,677,
+                117,822,
+                111,311,
+                95,740
+            ],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -22,14 +36,34 @@ const myChart = new Chart(ctx, {
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
             ],
-            borderWidth: 1
+            borderWidth: 1,
+            hoverBorderWidth:3,
+            hoverBorderColor:'black'
         }]
     },
     options: {
-        scales: {
-            y: {
-                beginAtZero: true
+        title: {
+            display:true,
+            text:'Largest Cities',
+            fontSize:25
+        },
+        legend:{
+            display:false,
+            position:'right',
+            labels:{
+             fontColor:'#000'
             }
+        },
+        layout:{
+            padding:{
+                left:50,
+                right:0,
+                bottom:0,
+                top:0
+            }
+        },
+        tooltips:{
+            enabled:true
         }
     }
 });
